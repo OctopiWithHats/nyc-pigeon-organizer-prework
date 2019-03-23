5336fccbc96191f148e04d025e4c3b0a2ac9a19b
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 15:19:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,50 +1,25 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  
+
   new_hash = {}
   
-  data.each do |first_level, second_level|
-    second_level.each do |category, array|
+  data.each do |property, hash|
+    hash.each do |attribute, array|
       array.each do |name|
-        new_hash[name] = {
-          :color => [],
-          :gender => [],
-          :lives => []}
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
         end
       end
     end
-    
-    x = new_hash.keys
-    data[:color].each do |bird_color, name|
-      name.each do |bird_name|
-        x.each do |item|
-          if bird_name == item
-            new_hash[item][:color] << bird_color.to_s
-          end
-        end
-      end
-    end
-    
-    data[:gender].each do |gender, type|
-      type.each do |bird_name|
-        x.each do |item|
-          if bird_name == item
-            new_hash[item][:gender] << gender.to_s
-          end
-        end
-      end
-    end
-    
-    data[:lives].each do |location, name|
-      name.each do |bird_name|
-        x.each do |item|
-          if bird_name == item
-            new_hash[item][:lives] << location.to_s
-          end
-        end
-      end
-    end
-    
-    new_hash
   end
+  new_hash
+end
 end
