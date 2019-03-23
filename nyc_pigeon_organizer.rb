56d1e0039adbf729128e479057e71d54ba94de0e
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 15:07:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,5 +6,6 @@ def nyc_pigeon_organizer(data)
     subhash.each do |attribute, array|
       array.each do |name|
         if !new_hash.has_key?(name)
-          new_hash[name] 
+          new_hash[name] = {}
+          
 end
