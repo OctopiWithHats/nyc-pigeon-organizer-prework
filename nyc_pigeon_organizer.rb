bf339667f1d61335d71dcabbd521fb624126b765
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 15:11:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,22 +2,6 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   new_hash = {}
-  data.each do |property, subhash|
-    subhash.each do |attribute, array|
-      array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
-        end
-        
-        if !new_hash.has_key?(property)
-          new_hash[name][property] = []
-        end
-        
-        if !new_hash[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
-        end
-      end
-    end
-  end
-  new_hash
+  
+  
 end
